7d312910a43884c8d90e025d51b56941de6bb2fc
Hinata-Nakano <0427practice@gmail.com>
2024-09-22 04:08:02 +0900

    反映した

--- dist/HandlegameOver.js
+++
@@ -1,7 +0,0 @@
-export function handleGameOver(currentTetromino, currentPosition, cells) {
-    if (currentTetromino.some(index => cells[index + currentPosition].classList.contains('fixed'))) {
-        alert('Game Over');
-        return true;
-    }
-    return false;
-}
--- dist/game.js
+++
@@ -1,3 +0,0 @@
-import { TetrisGame } from './Service/TetrisMoving.repository.js';
-const game = new TetrisGame();
-game.startGame();
